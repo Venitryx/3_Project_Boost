dd879a23bdd8173c0b518b09e5b6ede255cc2e90
Venitryx <47960552+Venitryx@users.noreply.github.com>
2019-03-12 22:27:15 -0400

    Fixed movement bugs
    changed camera distance, and fixed rotation bugs

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -23,20 +23,33 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+        Thrust();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; //freeze physics
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back);
+        }
+
+        rigidbody.freezeRotation = false; //resume physics
+    }
+
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
             if (!audioSource.isPlaying) audioSource.Play();
         }
         else audioSource.Stop();
-
-        if(Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.back); 
-        }
     }
 }
