4b07daedd76bff96808974f8e3923380320b109b
Venitryx <47960552+Venitryx@users.noreply.github.com>
2019-03-24 22:49:08 -0400

    Finished section 3, lesson 61.
    Added moving obstacles, also modified fake obstacle at the beginning of level 1.

---
+++ 3_Project_Boost/Assets/Oscillator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float period = 2f;
+    //todo remove from inspector later
+    [Range(0, 1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPos;
+    [SerializeField] Vector3 endPos;
+    Vector3 movementVector;
+
+    void Start()
+    {
+        startingPos = transform.position;
+        movementVector = endPos - startingPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
